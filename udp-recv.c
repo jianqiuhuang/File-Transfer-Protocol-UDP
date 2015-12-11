e0857300b2c4360ba1f3e09e1ad1ea9a7f1bed6f
Jianqiu Huang <jhuang86@cs.binghamton.edu>
2015-12-10 19:29:16 -0500

    first

---
+++ udp-recv.c
@@ -0,0 +1,119 @@
+/*
+    Demo-udp-03: udp-recv: a simple udp server
+	receive udp messages
+
+        usage:  udp-recv
+
+        Paul Krzyzanowski
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "port.h"
+
+#define BUFSIZE 2048
+#define DEBUG 1
+#define SIZE 5
+int
+main(int argc, char **argv)
+{
+	struct sockaddr_in myaddr;	/* our address */
+	struct sockaddr_in remaddr;	/* remote address */
+	socklen_t addrlen = sizeof(remaddr);		/* length of addresses */
+	int recvlen;			/* # bytes received */
+	int fd;				/* our socket */
+	int msgcnt = 0;			/* count # of messages we received */
+	unsigned char buf[BUFSIZE];	/* receive buffer */
+
+
+	/* create a UDP socket */
+
+	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("cannot create socket\n");
+		return 0;
+	}
+
+	/* bind the socket to any valid IP address and a specific port */
+
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
+    //bind to a specified address and port number
+//    myaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//    myaddr.sin_port = htons(1333);
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myaddr.sin_port = htons(SERVICE_PORT);
+
+	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}
+
+    /* receive file name and file size */
+    char *fileName = NULL;
+    unsigned long long fileSize = -1;
+    recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+    if (recvlen > 0){
+        buf[recvlen] = 0;
+        fileName = buf;
+        if(DEBUG) printf("File name is: %s\n", buf);
+    }    
+    recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+    if (recvlen > 0){
+        buf[recvlen] = 0;
+        fileSize = (unsigned long long)atoi(buf);
+        if(DEBUG) printf("File size is: %llu\n", fileSize);
+    }    
+
+    /* check if fileName and fileSize have been set */
+    if(fileName == NULL || fileSize == -1){
+        perror("Did not receive file name or file size info");
+        return 0;
+    }
+
+    /* create file stream and allocate temporary data storage array */
+    FILE *fp;
+    fp = fopen(fileName, "w");
+    if(fp == NULL){
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    int arrSize = fileSize / SIZE;
+    if(fileSize % SIZE != 0)
+        arrSize+=1;
+
+    /* allocating memory for temporary data storage */
+    char** stringList = (char**)malloc(arrSize * sizeof(char*));
+    for(int i = 0; i < arrSize; ++i){
+        stringList[i] = (char*)malloc(SIZE);
+    }
+
+	/* now loop, receiving data and printing what we received */
+	while(1) {
+		printf("waiting on port %d\n", SERVICE_PORT);
+		recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+		if (recvlen > 0) {
+			buf[recvlen] = 0;
+			printf("received message: \"%s\" (%d bytes)\n", buf, recvlen);
+            int index = buf[strlen(buf)-1] - '0';
+            buf[strlen(buf)-1] = 0;
+            printf("index: %d, %s\n", index, buf);
+            //strncpy(stringList[buf[SIZE-1] - '0'], buf, SIZE)
+		}
+		else
+			printf("uh oh - something went wrong!\n");
+		sprintf(buf, "ack %d", msgcnt++);
+		printf("sending response \"%s\"\n", buf);
+		if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, addrlen) < 0)
+			perror("sendto");
+        printf("printing stringlist\n");
+        for(int i = 0; i < arrSize; ++i){
+            printf("%s\n", stringList[i]);
+        }
+    }
+    fclose(fp);
+	/* never exits */
+}
---
+++ udp-send.c
@@ -0,0 +1,121 @@
+/*
+        demo-udp-03: udp-send: a simple udp client
+	send udp messages
+	This sends a sequence of messages (the # of messages is defined in MSGS)
+	The messages are sent to a port defined in SERVICE_PORT 
+
+        usage:  udp-send
+
+        Paul Krzyzanowski
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include "port.h"
+
+#define BUFLEN 2048
+#define MSGS 5	/* number of messages to send */
+#define DEBUG 1
+#define SIZE 5
+int main(void)
+{
+	struct sockaddr_in myaddr, remaddr;
+	int fd, i, slen=sizeof(remaddr);
+	char buf[BUFLEN];	/* message buffer */
+	int recvlen;		/* # bytes in acknowledgement message */
+	char *server = "127.0.0.1";	/* change this to use a different server */
+
+	/* create a socket */
+
+	if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1)
+		printf("socket created\n");
+
+	/* bind it to all local addresses and pick any port number */
+
+	memset((char *)&myaddr, 0, sizeof(myaddr));
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myaddr.sin_port = htons(0);
+
+	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}       
+
+	/* now define remaddr, the address to whom we want to send messages */
+	/* For convenience, the host address is expressed as a numeric IP address */
+	/* that we will convert to a binary format via inet_aton */
+
+	memset((char *) &remaddr, 0, sizeof(remaddr));
+	remaddr.sin_family = AF_INET;
+	remaddr.sin_port = htons(SERVICE_PORT);
+	if (inet_aton(server, &remaddr.sin_addr)==0) {
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+
+    /* open the file */
+    FILE *fp;
+    fp = fopen("tmp/in.txt", "r");
+    if(fp == NULL){
+        perror("fopen");
+        exit(1);
+    }
+    /* determine file size */
+    fseek(fp, 0L, SEEK_END);
+    unsigned long long fileSize  = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+    if(DEBUG) printf("File size: %d\n", fileSize);
+
+/*    while(fgets(buff, 5, fp)){
+        printf("%s\n", buff);
+    }
+*/
+	/* now let's send the messages */
+
+    /* first send the file name */
+    sprintf(buf, "in.txt");
+    if(sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1){
+        perror("sendto");
+        exit(1);
+    }
+    /* send file size */
+    sprintf(buf, "%llu", fileSize);
+    if(sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1){
+        perror("sendto");
+        exit(1);
+    }
+
+    /* transmit file data */
+    i = 0;
+    // integer argument for reading length n-1
+	while(fgets(buf, SIZE, fp)) {
+        if(DEBUG){
+            printf("*****Before sprintf, buf contains: %s\n", buf);
+		    printf("*****Sending packet %d to %s port %d\n", i, server, SERVICE_PORT);
+        }
+//		sprintf(buf, "%s%d", buf, i);
+        /* append sequence number to end of message */
+        buf[strlen(buf)] = i + '0';
+        if(DEBUG)
+            printf("*****After sprintf, buf contains: %s\n", buf);
+		if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1) {
+			perror("sendto");
+			exit(1);
+		}
+        bzero(buf, BUFLEN);
+        /* now receive an acknowledgement from the server */
+		recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &slen);
+                if (recvlen >= 0) {
+                        buf[recvlen] = 0;	/* expect a printable string - terminate it */
+                        printf("received message: \"%s\"\n", buf);
+                }
+        bzero(buf, BUFLEN);
+        i++;
+	}
+	close(fd);
+	return 0;
+}
