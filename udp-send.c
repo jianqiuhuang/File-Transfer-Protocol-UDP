a8d839fd36a736c75de9c0574c1013e197289a5b
Jianqiu Huang <jhuang86@cs.binghamton.edu>
2015-12-10 20:20:19 -0500

    .

--- udp-send.c
+++ udp-send.c
@@ -16,7 +16,7 @@
 #include <sys/socket.h>
 #include "port.h"
 
-#define BUFLEN 2048
+#define BUFLEN 512
 #define MSGS 5	/* number of messages to send */
 #define DEBUG 1
 #define SIZE 5
@@ -24,7 +24,7 @@ int main(void)
 {
 	struct sockaddr_in myaddr, remaddr;
 	int fd, i, slen=sizeof(remaddr);
-	char buf[BUFLEN];	/* message buffer */
+	unsigned char buf[BUFLEN];	/* message buffer */
 	int recvlen;		/* # bytes in acknowledgement message */
 	char *server = "127.0.0.1";	/* change this to use a different server */
 
@@ -59,7 +59,7 @@ int main(void)
 
     /* open the file */
     FILE *fp;
-    fp = fopen("tmp/in.txt", "r");
+    fp = fopen("in.txt", "rb");
     if(fp == NULL){
         perror("fopen");
         exit(1);
@@ -70,10 +70,6 @@ int main(void)
     fseek(fp, 0L, SEEK_SET);
     if(DEBUG) printf("File size: %d\n", fileSize);
 
-/*    while(fgets(buff, 5, fp)){
-        printf("%s\n", buff);
-    }
-*/
 	/* now let's send the messages */
 
     /* first send the file name */
@@ -91,31 +87,49 @@ int main(void)
 
     /* transmit file data */
     i = 0;
+    bzero(buf, BUFLEN);
     // integer argument for reading length n-1
-	while(fgets(buf, SIZE, fp)) {
+    while(1){
+	    fread(buf, SIZE, 1, fp);
+        if(strlen(buf) == 0){
+            printf("size zero\n");
+            break;
+        }
         if(DEBUG){
-            printf("*****Before sprintf, buf contains: %s\n", buf);
-		    printf("*****Sending packet %d to %s port %d\n", i, server, SERVICE_PORT);
+//            printf("*****Before sprintf, buf contains: %s\n", buf);
+//		    printf("*****Sending packet %d to %s port %d\n", i, server, SERVICE_PORT);
         }
 //		sprintf(buf, "%s%d", buf, i);
         /* append sequence number to end of message */
         buf[strlen(buf)] = i + '0';
+
         if(DEBUG)
             printf("*****After sprintf, buf contains: %s\n", buf);
-		if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1) {
+		
+        if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1) {
 			perror("sendto");
 			exit(1);
 		}
+        
         bzero(buf, BUFLEN);
+        
         /* now receive an acknowledgement from the server */
 		recvlen = recvfrom(fd, buf, BUFLEN, 0, (struct sockaddr *)&remaddr, &slen);
                 if (recvlen >= 0) {
                         buf[recvlen] = 0;	/* expect a printable string - terminate it */
                         printf("received message: \"%s\"\n", buf);
                 }
+        
         bzero(buf, BUFLEN);
         i++;
+	
+    }
+    /* final empty packet so that the server knows the transmission is done */
+    if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1) {
+	    perror("sendto");
+		exit(1);
 	}
+
 	close(fd);
 	return 0;
 }
